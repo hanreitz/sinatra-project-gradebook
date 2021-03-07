553c09577803532a8e53c2451429d77cafb958c9
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-03-07 18:28:12 -0500

    edits to assignment CRUD

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -1,30 +1,67 @@
 class AssignmentController < ApplicationController
 
-  get '/assignment/new' do
+  get '/courses/:id/assignments' do
+    @course = Course.find_by(id: params[:id])
+    redirect to "/courses/#{@course.id}"
+  end
+
+  get '/courses/:id/assignments/new' do
+    @course = Course.find_by(id: params[:id])
     erb :'assignments/new'
   end
 
-  get '/assignment/:id' do
-    @assignment = Assignment.find_by(params[:id])
+  get '/courses/:course_id/assignments/:id' do
+    @course = Course.find_by(id: params[:course_id])
+    @assignment = Assignment.find_by(id: params[:id])
     erb :'assignments/show'
   end
 
-  get '/assignments/:id/edit' do
-    @assignment = Assignment.find_by(params[:id])
+  get '/courses/:course_id/assignments/:id/edit' do
+    @course = Course.find_by(id: params[:course_id])
+    @assignment = Assignment.find_by(id: params[:id])
     erb :'assignments/edit'
   end
 
-  post '/assignments' do
-    @assignment = Assignment.create(params[:course])
-    @course.teacher = Teacher.find_by(id: session[:user_id])
-    student = Student.new(params[:student])
-    if student.save
-      @course.students << student
-      @course.save
+  post '/courses/:id/assignments' do
+    @course = Course.find_by(id: params[:assignment][:course_id])
+    if !params[:assignment][:name].empty?
+      @assignment = Assignment.create(params[:assignment])
+      @assignment.course = @course
       redirect to "/courses/#{@course.id}"
     else
-      flash[:message] = "New student(s) could not be saved. Name field is required."
-      redirect to '/courses/new'
+      flash[:message] = "ASSIGNMENT NOT CREATED. Name is required."
+      redirect to "/courses/#{@course.id}"
+    end
+  end
+
+  patch '/courses/:course_id/assignments/:id' do
+    @course = Course.find_by(id: params[:course_id])
+    @assignment = Assignment.find_by(id: params[:id])
+    if @assignment.update(params[:assignment])
+      @assignment.update(params[:assignment])
+      @course.students.each do |student|
+        if !params[:student][:grade].empty?
+          student.student_assignments.find_by(assignment_id: @assignment.id).grade.update(params[:student][:grade])
+        end
+      end
+      flash[:message] = "Assignment successfully updated!"
+      redirect to "/courses/#{@course.id}"
+    else
+      flash[:message] = "Assignment must have a name."
+      redirect to "/courses/#{@course.id}/assignments/#{@assignment.id}"
+    end
+  end
+
+  delete '/courses/:course_id/assignments/:id' do
+    @course = Course.find_by(id: params[:course_id])
+    @assignment = Assignment.find_by(id: params[:id])
+    if @course.teacher_id == session[:user_id]
+      @assignment.destroy
+      flash[:message] = "Assignment successfully removed."
+      redirect "/courses/#{@course.id}"
+    else
+      flash[:message] = "You do not have access to this function."
+      redirect '/courses'
     end
   end
 
